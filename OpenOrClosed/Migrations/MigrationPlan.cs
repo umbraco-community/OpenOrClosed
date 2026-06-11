47e863d0013fbbaed1873d4570b718008a909f74
Robert Foster <robert@youritgroup.com.au>
2026-06-11 14:40:25 +0200

    Fix PackageMigration Key

--- OpenOrClosed/Migrations/MigrationPlan.cs
+++ OpenOrClosed/Migrations/MigrationPlan.cs
@@ -3,7 +3,7 @@ using Umbraco.Cms.Core.Packaging;
 
 namespace OpenOrClosed.Migrations;
 
-internal sealed class MigrationPlan() : PackageMigrationPlan("GMaps")
+internal sealed class MigrationPlan() : PackageMigrationPlan("OpenOrClosed")
 {
     public override string InitialState => "{openorclosed-init-state}";
 
